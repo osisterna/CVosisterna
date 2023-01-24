d8b50925bbe7331bc277d9e8b2a200f12041fa57
Oscar Sisterna <osisterna@gmail.com>
2023-01-24 14:45:10 -0300

    Commit inicial

---
+++ ejemplo.js
@@ -0,0 +1,17 @@
+/**
+    hour = 19;
+    if (hour < 18) {
+        greeting = "Good day";
+    } else {
+        greeting = "Good evening";
+    }
+    */
+
+    time = 11;
+    if (time < 10) {
+        greeting = "Good morning";
+      } else if (time < 20) {
+        greeting = "Good day";
+      } else {
+        greeting = "Good evening";
+      }
